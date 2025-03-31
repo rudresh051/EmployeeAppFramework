623a74203c93a23c26309758f24f0163c338864f
Rudreshwar Jha <jharudreshwar17@gmail.com>
2025-03-31 16:10:18 +0530

    Implemented Page Navigation with Generics - Lecture 19

--- src/com/kritikal/framework/base/Base.java
+++ src/com/kritikal/framework/base/Base.java
@@ -1,7 +1,15 @@
 package com.kritikal.framework.base;
 
+import org.openqa.selenium.support.PageFactory;
+
 public class Base {
 
     public static BasePage CurrentPage;
+
+    public <TPage extends BasePage> TPage GetInstance(Class<TPage> page)
+    {
+        Object obj = PageFactory.initElements(DriverContext.Driver,page);
+        return page.cast(obj);
+    }
 
 }
--- src/com/kritikal/framework/base/BasePage.java
+++ src/com/kritikal/framework/base/BasePage.java
@@ -3,8 +3,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 public abstract class BasePage extends Base{
 
-    public BasePage() {
+/*    public BasePage() {
         PageFactory.initElements(DriverContext.Driver, this);
+    }*/
+
+    public <TPage extends BasePage> TPage As(Class<TPage> pageInstance)
+    {
+        try{
+            return (TPage) this;
+        }
+        catch (Exception e){
+            e.getStackTrace();
+        }
+        return null;
     }
 
 
